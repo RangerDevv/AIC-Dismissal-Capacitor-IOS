0a0729fe27eb6403dc448383c0490a4c18b3f997
RangerDevv <meerabdul2008@gmail.com>
2024-02-08 01:22:59 -0500

    added capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.aicdismissal.vercel',
+  appName: 'SimpleDismiss',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;

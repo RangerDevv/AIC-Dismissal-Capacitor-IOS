98a2256242f8f9585e29c38ad4ad4c937ce74dd7
RangerDevv <meerabdul2008@gmail.com>
2024-02-08 01:04:58 -0500

    Initial scaffold

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import "../app.pcss";
+    import { setupIonicBase } from 'ionic-svelte';
+
+    /* Theme variables */
+    import '../theme/variables.css';
+
+    /* load and register all components - you can also import separately to code split */
+    import 'ionic-svelte/components/all';
+
+    /* run base configuration code from ionic/core */
+    setupIonicBase();
+</script>
+
+<ion-app>
+   <slot></slot>
+</ion-app>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,1 @@
+Hi there <ion-button>test</ion-button>
